0a37642b3e3692cea87ea87984532ecde07b8e9f
brianshl007 <brianshlee98@gmail.com>
2025-12-02 16:06:07 -0500

    Added functionalities to operators, equals, and clear. Added functions to calculate result

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function divide(a, b) {
 let first = '';
 let second = '';
 let operator = '';
-let displayValue = '';
+let displayValue = '0';
 
 
 //Function to perform operation based on what type of math to calculate
@@ -67,4 +67,63 @@ numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         inputNumber(button.textContent);
     });
+});
+
+//Function to handle operator inputs
+function inputOperator(op) {
+    if (first === '') {
+        first = displayValue;
+        operator = op;
+        displayValue = '0';
+    }   else if (operator && displayValue !== '0') {
+        second = displayValue;
+        first = String(operate(operator, parseFloat(first), parseFloat(second)));
+        operator = op;
+        displayValue = first;
+        updateDisplay();
+        displayValue = '0';
+    }   else {
+        operator = op;
+    } 
+}
+
+//Function to calculate result with equals button
+function calculate() {
+    if (first !== '' && operator !== '' && displayValue !== '0') {
+        second = displayValue;
+        const result = operate(operator, parseFloat(first), parseFloat(second));
+        displayValue = String(result);
+        updateDisplay();
+        first = '';
+        second = '';
+        operator = '';  
+    }
+}
+
+//Function to clear all values
+function clear() {
+    first = '';
+    second = '';
+    operator = '';
+    displayValue = '0';
+    updateDisplay();
+}
+
+//Adding event listeners to operator, equals and clear buttons
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        inputOperator(button.textContent);
+    });
+});
+
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', () => {
+    calculate();
+});
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    clear();
 });
