ac2172ce94aa176dfd6622758f892d4e7509fa34
brianshl007 <brianshlee98@gmail.com>
2025-12-02 15:56:01 -0500

    Added button functionality for numbers

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('Script loaded!');
+
 //Adding basic math functions here
 function add(a, b) {
     return a + b;
@@ -19,9 +21,11 @@ function divide(a, b) {
 }
 
 //initializing variables to hold values and operator
-let first = null;
-let second = null;
-let operator = null;
+let first = '';
+let second = '';
+let operator = '';
+let displayValue = '';
+
 
 //Function to perform operation based on what type of math to calculate
 
@@ -40,3 +44,27 @@ function operate(operator, a, b) {
     }
 }
 
+//Get the display element
+const display = document.querySelector('.display');
+
+//Function to update display value
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+//Function to handle number inputs
+function inputNumber(num) {
+    if (displayValue === '0') {
+        displayValue = num;
+    } else {
+        displayValue += num;
+    } updateDisplay();
+}
+
+//Adding button capabilities to all the buttons
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        inputNumber(button.textContent);
+    });
+});
